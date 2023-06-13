5b6b7430b9e474c26e54f984327b735a05a65966
talha073 <talhajavedd@gmail.com>
2023-06-13 16:09:17 +0400

    add mint function

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -12,4 +12,11 @@ contract RealEstate is ERC721URIStorage {
    Counters.counter private _tokenIds;
 
     constructor() ERC721("Real Estate", "REAL") {}
+    function mint(string memory tokenURI) public returns(uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
 }
