28a09d51faf854f5dcc032a6681c04ee89862984
talha073 <talhajavedd@gmail.com>
2023-06-13 16:20:27 +0400

    Escrow setup

---
+++ contracts/Escrow.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(address _from, address _to, uint256 id) external;
+}
+
+contract Escrow {
+ 
+    constructor() {
+        
+    }
+}
