42aee4b3e7040c98fe94a4825d7021a44f21062a
talha073 <talhajavedd@gmail.com>
2023-06-14 16:40:25 +0400

    test return address

--- test/Escrow.test.js
+++ test/Escrow.test.js
@@ -5,28 +5,45 @@ const token = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
-describe("Escrow", () => {
-  let buyer, seller, inspector, lender, realEstate, escrow;
+describe("Escrow contract", () => {
+  let buyer, seller, inspector, lender;
+
   it("save the address", async () => {
     [buyer, seller, inspector, lender] = await ethers.getSigners();
 
     const RealEstate = await ethers.getContractFactory("RealEstate");
-    realEstate = await RealEstate.deploy();
-    console.log("contract deployed at: ", realEstate.address);
+    const realEstate = await RealEstate.deploy();
 
     let tx = await realEstate.connect(seller).mint("www.google.com");
     await tx.wait();
 
     const Escrow = await ethers.getContractFactory("Escrow");
-    console.log("33");
-    escrow = await Escrow.deploy(
+    const escrow = await Escrow.deploy(
       realEstate.address,
       seller.address,
       inspector.address,
       lender.address
     );
+  });
+  describe("Deployment", () => {
+    it("Returns NFT address", async () => {
+      const result = await escrow.nftAddress();
+      expect(result).to.be.equal(realEstate.address);
+    });
 
-    let result = await escrow.nftAddress();
-    expect(result).to.be.equal(realEstate.address);
+    it("Returns seller", async () => {
+      const result = await escrow.seller();
+      expect(result).to.be.equal(seller.address);
+    });
+
+    it("Returns inspector", async () => {
+      const result = await escrow.inspector();
+      expect(result).to.be.equal(inspector.address);
+    });
+
+    it("Returns lender", async () => {
+      const result = await escrow.lender();
+      expect(result).to.be.equal(lender.address);
+    });
   });
 });
