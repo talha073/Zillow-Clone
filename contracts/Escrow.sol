fffde83c3a92d508b8c360b5c100e6e9f25e3e90
talha073 <talhajavedd@gmail.com>
2023-06-22 20:45:04 +0400

    cancelSale

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -71,4 +71,11 @@ contract Escrow {
         // Transfer NFT to new Owner and Escrow Amount back to Lender  
          IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
     }
+     function cancelSale(uint256 _nftID) public {
+        if (inspectionPassed[_nftID] == false) {
+            payable(buyer[_nftID]).transfer(address(this).balance);
+        } else {
+            payable(seller).transfer(address(this).balance);
+        }
+    }
 }
