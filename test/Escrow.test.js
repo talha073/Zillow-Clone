0806fd47b28265105e2e864ff603fdd91058345a
talha073 <talhajavedd@gmail.com>
2023-06-24 17:58:57 +0400

    test cases of approval

--- test/Escrow.test.js
+++ test/Escrow.test.js
@@ -96,4 +96,16 @@ describe("Escrow", () => {
       expect(result).to.be.equal(true);
     });
   });
+  describe("Approval", () => {
+    beforeEach(async () => {
+      let transaction = await escrow.connect(buyer).approveSale(1);
+      await transaction.wait();
+
+      transaction = await escrow.connect(seller).approveSale(1);
+      await transaction.wait();
+
+      transaction = await escrow.connect(lender).approveSale(1);
+      await transaction.wait();
+    });
+  });
 });
