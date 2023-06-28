b1d94373f3702a7578377005f02485c2bfae7e75
talha073 <talhajavedd@gmail.com>
2023-06-28 22:31:56 +0400

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  solidity: "0.8.3",
 };
