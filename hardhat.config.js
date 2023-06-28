257181bde2ba826896799c76b6ffa909c783e0de
talha073 <talhajavedd@gmail.com>
2023-06-28 22:32:22 +0400

    add gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,4 +25,12 @@ module.exports = {
     },
   },
   solidity: "0.8.3",
+
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
 };
